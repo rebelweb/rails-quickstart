760244015a5df332bc79448ab7bffff2ea0bba71
rebelweb <ryan@ryancondronphotography.com>
2015-06-06 11:25:08 -0500

    Added Settings File and Initializer

--- rails-quickstart.rb
+++ rails-quickstart.rb
@@ -1,9 +1,15 @@
 puts "Generating with Rails Quickstart Template"
 
+#Add this folder to path temporarily
 def source_paths
   Array(super) +
     [File.expand_path(File.dirname(__FILE__))]
 end
 
+#Replace Default Gemfile
 remove_file 'Gemfile'
 copy_file 'quickstart-Gemfile', 'Gemfile'
+
+#Load Settings File
+copy_file 'settings.yml', 'config/settings.yml'
+copy_file 'load_settings.rb', 'config/initializers/load_settings.rb'
