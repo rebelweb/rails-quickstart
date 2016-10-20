2a2f983124e78f11a4d6f99166c2ae0f64fb748d
Ryan <rebelwebdevelopment@gmail.com>
2016-10-19 21:32:09 -0500

    added initial git commit

--- rails-quickstart.rb
+++ rails-quickstart.rb
@@ -1,7 +1,12 @@
-puts "Generating with Rails Quickstart Template"
-
+puts "Setting Up Stuff Quickly"
 #Add this folder to path temporarily
 def source_paths
   Array(super) +
     [File.expand_path(File.dirname(__FILE__))]
+end
+
+after_bundle do
+  git :init
+  git add: '.'
+  git commit: "-a -m 'Initial commit'"
 end
