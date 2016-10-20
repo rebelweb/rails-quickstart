f9d86e586db242e3f8e37a9124e991ff316c2060
Ryan <rebelwebdevelopment@gmail.com>
2016-10-19 22:02:02 -0500

    setup dotenv-rails

--- rails-quickstart.rb
+++ rails-quickstart.rb
@@ -1,9 +1,21 @@
 puts "Setting Up Stuff Quickly"
+
 #Add this folder to path temporarily
 def source_paths
   Array(super) +
     [File.expand_path(File.dirname(__FILE__))]
 end
+
+#Setup Environment Variables
+gem 'dotenv-rails', group: [:development, :test]
+
+append_to_file ".gitignore", ".env"
+
+create_file '.env-example', <<-ENV
+DB_HOST='127.0.0.1'
+DB_USER='ryan'
+DB_PASS='Lightning.11'
+ENV
 
 after_bundle do
   git :init
