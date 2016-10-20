c1dc6ef9c0cc94201b8546bc07789dc4ce6768b0
Ryan <rebelwebdevelopment@gmail.com>
2016-10-19 22:24:26 -0500

    added rspec detup

--- rails-quickstart.rb
+++ rails-quickstart.rb
@@ -6,16 +6,30 @@ def source_paths
     [File.expand_path(File.dirname(__FILE__))]
 end
 
-#Setup Environment Variables
+# Setup Environment Variables
 gem 'dotenv-rails', group: [:development, :test]
 
 append_to_file ".gitignore", ".env"
 
 create_file '.env-example', <<-ENV
 DB_HOST='127.0.0.1'
-DB_USER='ryan'
-DB_PASS='Lightning.11'
+DB_USER=''
+DB_PASS=''
 ENV
+
+# RSpec Setup
+if yes?('Do you want to use RSpec?')
+  gem_group :development, :test do
+    gem 'rspec-rails'
+    gem 'rails-controller-testing'
+    gem 'factory_girl_rails'
+    gem 'capybara'
+    gem 'poltergeist'
+    gem 'simplecov'
+  end
+
+  generate('rspec:install')
+end
 
 after_bundle do
   git :init
